4f5cfffa19d9c101ab45649fba03224c96ce30ce
harisalkarni <haris.alkarni@gmail.com>
2022-11-08 14:27:21 +0700

    4th update

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
-const CountryTracks = () => <div>CountryTracks</div>;
+import { Error, Loader, SongCard } from '../components';
 
-export default CountryTracks;
+const AroundYou = () => {
+
+    const [country, setCountry] = useState("");
+    const [loading, setLoading] = useState(true);
+    const { activeSong, isPlaying} = useSelector((state) => state.player)
+
+    console.log(country);
+
+    useEffect(() => {
+        axios.get(`https://geo.ipify.org/api/v2/country?apiKey=at_d45nCYv0hYyfluThmlzJl9xSexgus`)
+        .then((res) => setCountry(res?.data?.location?.country))
+        .catch((err) => console.log(err))
+        .finally(() => setLoading(false))
+    }, [country])
+
+    return (
+        <div>
+
+        </div>
+    )
+};
+
+export default AroundYou;
