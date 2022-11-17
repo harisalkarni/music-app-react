ddc9a6d882995e2f058bfdc898770430cb756ef8
harisalkarni <haris.alkarni@gmail.com>
2022-11-17 15:50:07 +0700

    6th update

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -17,6 +17,8 @@ import { createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
             getSongRelated: builder.query({query: ({songid}) => `/tracks/related?track_id=${songid}`}),
             getArtistDetails: builder.query({query: (artistId) => `/artists/details?artist_id=${artistId}`}),
             getSongsByCountry: builder.query({query: (countryCode) => `/charts/country?country_code=${countryCode}`}),
+            getSongsByGenre: builder.query({query: (genre) => `/charts/genre-world?genre_code=${genre}`}),
+            getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
         }),
     });
 
@@ -26,4 +28,6 @@ export const {
     useGetSongRelatedQuery,
     useGetArtistDetailsQuery,
     useGetSongsByCountryQuery,
+    useGetSongsByGenreQuery,
+    useGetSongsBySearchQuery,
 } = shazamCoreApi;
